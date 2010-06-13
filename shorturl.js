e381e4d5ec439bf62879443bcb625bc07d234070
orangkucing <orangkucing.hitam@gmail.com>
2010-06-14 06:31:47 +0900

    utilize arguments in execJson()

--- shorturl.js
+++ shorturl.js
@@ -4,8 +4,6 @@
 // we need a CGI server to pad a callback function name.
 const tunnel = "http://onecotravel.info/cgi-bin/tumblr/tunnel.cgi"; // CHANGE HERE IF NEEDED
 
-const user_timeline = "http://www.tumblr.com/statuses/user_timeline.json";
-const apipath = "/api/read/json";
 const watermark = "Tumblr permalink...";
 const msgs = {
     notumblelog: "This URL is not a Tumblr blog...",
@@ -13,10 +11,13 @@ const msgs = {
     woops: "Woops. Something strange happened..."
 };
 
-K.execJson = function (u) {
+K.execJson = function () {
+    var i;
     var s = document.createElement("script");
     s.type = "text/javascript";
-    s.src = tunnel + "?api=" + u.replace("?", "&");
+    s.src = tunnel + "?";
+    for (i = 0; i < arguments.length; i++)
+        s.src += arguments[i] + "=" + K[arguments[i]] + "&";
     document.getElementsByTagName("body")[0].appendChild(s);
 }
 
@@ -25,11 +26,11 @@ K.show = function (s) {
         return c && "<div>" + s + "</div>";
     }
     document.getElementById(K.myname + "_buf").innerHTML =
-    w(K.screenName, "name: " + K.screenName + (K.userId &&  " (" + K.userId + ")")) +
+    w(K.screen_name, "name: " + K.screen_name + (K.userId &&  " (" + K.userId + ")")) +
     w(K.reblogKey, "reblog key: " + K.reblogKey) +
     w(K.shortURLPrefix,
         "short URL: <input type=\"text\" readonly=\"readonly\" onclick=\"this.select();\" style=\"width:190px;\" " +
-        "value=\"http://tumblr.com/x" + K.shortURLPrefix + parseInt(K.postId, 10).toString(36) + "\">") +
+        "value=\"http://tumblr.com/x" + K.shortURLPrefix + parseInt(K.id, 10).toString(36) + "\">") +
     w(K.statusId, "status ID: " + K.statusId) +
     w(s, msgs[s]);
 }
@@ -48,7 +49,7 @@ K.P = function (obj) {
     }
     for (i = 0; i < obj.length; i++) {
         if (f && !K.statusId)
-            if (parseInt(K.postId, 10) == parseInt(obj[i].id / 65536, 10)) {
+            if (parseInt(K.id, 10) == parseInt(obj[i].id / 65536, 10)) {
                 K.statusId = obj[i].id;
                 if (K.shortURLPrefix) {
                     K.show("");
@@ -70,7 +71,11 @@ K.P = function (obj) {
         K.show("woops");
         return;
     }
-    K.execJson(user_timeline + "?screen_name=" + K.screenName + "&callback=" + K.myname + ".P&count=200&page=" + ++K.page);
+    K.callback = K.myname + ".P";
+    K.count = 200;
+    K.page++;
+    K.api = "http://www.tumblr.com/statuses/user_timeline.json";
+    K.execJson("api", "screen_name", "callback", "count", "page");
 }
 
 K.SN = function (obj) {
@@ -78,7 +83,7 @@ K.SN = function (obj) {
         K.show("notumblelog");
         return;
     }
-    K.screenName = obj.tumblelog.name;
+    K.screen_name = obj.tumblelog.name;
     K.reblogKey = obj.posts[0]["reblog-key"];
     K.show("loading");
     K.page = 0;
@@ -88,7 +93,7 @@ K.SN = function (obj) {
 K.result = function () {
     K.statusId = "";
     K.shortURLPrefix = "";
-    K.screenName = "";
+    K.screen_name = "";
     K.reblogKey = "";
     K.userId = "";
     var m = document[K.myname + "_inputform"].url.value.match(/^(.*)\/post\/([\d]+)/);
@@ -97,9 +102,11 @@ K.result = function () {
         return;
     }
     var basename = m[1];
-    K.postId = m[2];
+    K.id = m[2];
     K.show("loading");
-    K.execJson(basename + apipath + "?callback=" + K.myname + ".SN&id=" + K.postId);
+    K.callback = K.myname + ".SN";
+    K.api = basename + "/api/read/json";
+    K.execJson("api",  "callback", "id");
 }
 
 document.write(
